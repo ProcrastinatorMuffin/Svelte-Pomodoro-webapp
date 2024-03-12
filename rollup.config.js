cde36ffa6b785f98642f1496821b6b6077251ba2
procrastinatorMuffin <danil.sholudko@icloud.com>
2024-03-12 13:53:52 +0100

    Update dependencies and styles

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import { spawn } from 'child_process';
 import svelte from 'rollup-plugin-svelte';
-import commonjs from '@rollup/plugin-commonjs';
-import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
+import { terser } from 'rollup-plugin-terser';
 import css from 'rollup-plugin-css-only';
+import sveltePreprocess from 'svelte-preprocess';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -39,37 +40,19 @@ export default {
 	},
 	plugins: [
 		svelte({
+			preprocess: sveltePreprocess(),
 			compilerOptions: {
-				// enable run-time checks when not in production
 				dev: !production
 			}
 		}),
-		// we'll extract any component CSS out into
-		// a separate file - better for performance
 		css({ output: 'bundle.css' }),
-
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration -
-		// consult the documentation for details:
-		// https://github.com/rollup/plugins/tree/master/packages/commonjs
 		resolve({
 			browser: true,
-			dedupe: ['svelte'],
-			exportConditions: ['svelte']
+			dedupe: ['svelte']
 		}),
 		commonjs(),
-
-		// In dev mode, call `npm run start` once
-		// the bundle has been generated
 		!production && serve(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
 		!production && livereload('public'),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
 		production && terser()
 	],
 	watch: {
--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,31 @@
 <script>
-	import { onMount } from 'svelte';
-	import Timer from './Timer.svelte';
-  
-	onMount(() => {
-	  if (Notification.permission !== "granted") {
-		Notification.requestPermission();
-	  }
-	});
-  </script>
-  
-  <style>
-	main {
-	  display: flex;
-	  justify-content: center;
-	  align-items: center;
-	  height: 100vh;
-	  background-color: #282a36; /* a soothing dark grey color */
-	  color: #f8f8f2; /* a light off-white color */
-	  font-family: 'Orbitron', sans-serif;
-	}
-  </style>
-  
-  <main>
+		import { onMount } from 'svelte';
+		import Timer from './Timer.svelte';
+	
+		onMount(() => {
+			if (Notification.permission !== "granted") {
+				Notification.requestPermission();
+			}
+		});
+</script>
+	
+<style lang="scss">
+$background-color: #282a36; // a soothing dark grey color
+$color: #f8f8f2; // a light off-white color
+$font-family: 'Orbitron', sans-serif;
+
+main {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 100vh;
+	background-color: $background-color;
+	color: $color;
+	font-family: $font-family;
+}
+</style>
+	
+<main>
 	<Timer />
-  </main>
+</main>
   
